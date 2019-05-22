7d1e681ff3df2c4879b52e230fb0aabc0f2dc166
姚枰 <yaoping@idongjia.cn>
2019-05-22 21:51:51 +0800

    日志

--- api/src/main/java/com/xieke/test/tyqxcms/log/converter/IpAddrConverter.java
+++ api/src/main/java/com/xieke/test/tyqxcms/log/converter/IpAddrConverter.java
@@ -2,6 +2,7 @@ package com.xieke.test.tyqxcms.log.converter;
 
 import ch.qos.logback.classic.pattern.ClassicConverter;
 import ch.qos.logback.classic.spi.ILoggingEvent;
+import com.xieke.test.tyqxcms.util.IpAddrUtil;
 
 /**
  * Created by yo
--- api/src/main/java/com/xieke/test/tyqxcms/util/IpAddrUtil.java
+++ api/src/main/java/com/xieke/test/tyqxcms/util/IpAddrUtil.java
@@ -46,7 +46,7 @@ public class IpAddrUtil {
 
     public static String getIp(HttpServletRequest request) {
         String ip = request.getHeader("X-Forwarded-For");
-        if (!isEmpty(ip) && !"unKnown".equalsIgnoreCase(ip)) {
+        if (!Utils.isEmpty(ip) && !"unKnown".equalsIgnoreCase(ip)) {
             //多次反向代理后会有多个ip值，第一个ip才是真实ip
             int index = ip.indexOf(",");
             if (index != -1) {
@@ -56,7 +56,7 @@ public class IpAddrUtil {
             }
         }
         ip = request.getHeader("X-Real-IP");
-        if (!isEmpty(ip) && !"unKnown".equalsIgnoreCase(ip)) {
+        if (!Utils.isEmpty(ip) && !"unKnown".equalsIgnoreCase(ip)) {
             return ip;
         }
         ip = request.getRemoteAddr();
